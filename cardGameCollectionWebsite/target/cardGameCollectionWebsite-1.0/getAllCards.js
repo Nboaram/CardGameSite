6472b99340bd12f2844cc1ff6904097441861306
Adrian Jones <nboaram1@hotmail.co.uk>
2019-06-11 08:17:52 +0100

    Made it ignore picture property

--- cardGameCollectionWebsite/target/cardGameCollectionWebsite-1.0/getAllCards.js
+++ cardGameCollectionWebsite/target/cardGameCollectionWebsite-1.0/getAllCards.js
@@ -7,7 +7,8 @@ function getAllCards() {
             for(let property in information[c]) {
                 console.log("Property= " + property);
                     console.log(c+"."+property);
-                    if (property != "id") {
+                    if (property != "id" && property != "pictureUrl") {
+                        console.log(property + " set.")
                     document.getElementById(c+"."+property).innerHTML = information[c][property];
                     }
             }
