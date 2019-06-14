ca5b79c387287d3db8cb01c874030737830c24c9
Adrian Jones <Nboaram@users.noreply.github.com>
2019-06-15 00:32:39 +0100

    Deck ID sent to deck page
    Deck ID in session storage on deckDetails ready for later.

--- cardGameCollectionWebsite/src/main/webapp/getAllDecksForUser.js
+++ cardGameCollectionWebsite/src/main/webapp/getAllDecksForUser.js
@@ -12,14 +12,21 @@ function getAllDecksFromUser(id) {
                 let newCell = newRow.insertCell(-1);
                 let newText = document.createTextNode(this.information[deck][variable])
                 newCell.appendChild(newText);
+                if (variable === "id"){
+                newCell.id = deck + ".id"; 
+                console.log(deck + ".id");
+                }
             }
             let newerCell = newRow.insertCell(-1);
             let button = document.createElement("button");
             button.innerHTML = "Deck Details";
             button.className = "btn btn-outline-info tableEntry";
             newerCell.appendChild(button);
+            console.log(deck+".id");
+            console.log(document.getElementById(deck+".id").innerHTML);
             button.addEventListener("click" , function() {
-            changePage('/cardGameCollectionWebsite-1.0/deckDetails.html')
+            sessionStorage.setItem('deckId', document.getElementById(deck+".id").innerHTML);
+            changePage('/cardGameCollectionWebsite-1.0/deckDetails.html');
             }); 
         }
     })
